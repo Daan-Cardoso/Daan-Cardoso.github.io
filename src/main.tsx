a02e68a3bb58b2ca4f388f8f5b4be3475901fe96
Danilo Cardoso <danilo.cardosolima@gmail.com>
2024-04-26 10:07:06 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/general/styles/reset.css'
+
+import App from './view/App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ src/view/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <h1>create project</h1>
+  )
+}
+
+export default App
